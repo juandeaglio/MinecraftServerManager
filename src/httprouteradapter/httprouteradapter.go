32d66753d0adf985ca96069279ab64eeb41e11a3
juandeaglio <casualgamer105@yahoo.com>
2025-03-23 13:24:35 -0400

    .r Moved the adapter code to src

---
+++ src/httprouteradapter/httprouteradapter.go
@@ -0,0 +1,30 @@
+package httprouteradapter
+
+import (
+	"io"
+	"minecraftremote/src/httprouter"
+	"net/http"
+)
+
+type HTTPRouterAdapter struct {
+	Router *httprouter.HTTPServer
+}
+
+func (a *HTTPRouterAdapter) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// Call your existing HandleHTTP method
+	response := a.Router.HandleHTTP(r)
+
+	// Transfer the response to the http.ResponseWriter
+	for key, values := range response.Header {
+		for _, value := range values {
+			w.Header().Add(key, value)
+		}
+	}
+	w.WriteHeader(response.StatusCode)
+
+	// If there's a response body, write it
+	if response.Body != nil {
+		io.Copy(w, response.Body)
+		response.Body.Close()
+	}
+}
--- tests/integration/godogs/control_server_steps_test.go
+++ tests/integration/godogs/control_server_steps_test.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"io"
 	"log"
 	"minecraftremote/src/controls/mcservercontrols"
 	"minecraftremote/src/httprouter"
+	"minecraftremote/src/httprouteradapter"
 	"net/http"
 	"testing"
 	"time"
@@ -16,51 +16,12 @@ type checkServerFeature struct {
 	router *httprouter.HTTPServer
 }
 
-// First, create an adapter type
-type HTTPHandlerAdapter struct {
-	router *httprouter.HTTPServer
-}
-
-// Implement the http.Handler interface
-func (a *HTTPHandlerAdapter) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	// Call your existing HandleHTTP method
-	response := a.router.HandleHTTP(r)
-
-	// Transfer the response to the http.ResponseWriter
-	for key, values := range response.Header {
-		for _, value := range values {
-			w.Header().Add(key, value)
-		}
-	}
-
-	w.WriteHeader(response.StatusCode)
-
-	// If there's a response body, write it
-	if response.Body != nil {
-		io.Copy(w, response.Body)
-		response.Body.Close()
-	}
-}
-
 func (c *checkServerFeature) theServerIsStarted() error {
 	controls := mcservercontrols.NewControls()
 	controls.Start()
 	c.router = httprouter.NewHTTPServer(controls)
-	adapter := &HTTPHandlerAdapter{router: c.router}
-	server := &http.Server{
-		Addr:         ":8080",
-		Handler:      adapter, // Use your router's HandleHTTP as the handler
-		ReadTimeout:  10 * time.Second,
-		WriteTimeout: 10 * time.Second,
-	}
-
-	// Start the server in a goroutine
-	go func() {
-		err := server.ListenAndServe()
-		if err != nil && err != http.ErrServerClosed {
-			log.Printf("HTTP server error: %v", err)
-		}
-	}()
+	routerAdapter := &httprouteradapter.HTTPRouterAdapter{Router: c.router}
+	startServerWithRouter(routerAdapter)
 
 	return nil
 }
@@ -101,4 +62,21 @@ func ClientAsksTheServerForTheStatusFeatureContext(s *godog.ScenarioContext) {
 	s.Given(`the server is started`, c.theServerIsStarted)
 	s.When(`a client asks the status`, c.aClientAsksTheStatus)
 	s.Then(`I should tell the client the status`, c.iShouldTellTheClientTheStatus)
+}
+
+func startServerWithRouter(adapter *httprouteradapter.HTTPRouterAdapter) {
+	server := &http.Server{
+		Addr:         ":8080",
+		Handler:      adapter, // Use your router's HandleHTTP as the handler
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	// Start the server in a goroutine
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
+			log.Printf("HTTP server error: %v", err)
+		}
+	}()
 }
