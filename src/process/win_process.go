1fc9fb3606f0f96a281728c5afb8c37f87b03e37
juandeaglio <casualgamer105@yahoo.com>
2025-03-28 22:22:07 -0400

    Separated out OSoperations from the process.

--- src/process/win_process.go
+++ src/process/win_process.go
@@ -7,10 +7,51 @@ import (
 	"syscall"
 )
 
+// OsOperations abstracts operating system level operations
+type OsOperations interface {
+	FindProcess(pid int) (*os.Process, error)
+	Signal(process *os.Process, signal syscall.Signal) error
+	CreateCommand(program string, args ...string) *exec.Cmd
+	SetSysProcAttr(cmd *exec.Cmd)
+	StartCmd(cmd *exec.Cmd) error
+	KillProcess(process *os.Process) error
+}
+
+// WindowsOsOperations implements OsOperations for Windows
+type WindowsOsOperations struct{}
+
+func (w *WindowsOsOperations) FindProcess(pid int) (*os.Process, error) {
+	return os.FindProcess(pid)
+}
+
+func (w *WindowsOsOperations) Signal(process *os.Process, signal syscall.Signal) error {
+	return process.Signal(signal)
+}
+
+func (w *WindowsOsOperations) CreateCommand(program string, args ...string) *exec.Cmd {
+	return exec.Command(program, args...)
+}
+
+func (w *WindowsOsOperations) SetSysProcAttr(cmd *exec.Cmd) {
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		CreationFlags: syscall.CREATE_NEW_PROCESS_GROUP,
+		HideWindow:    false,
+	}
+}
+
+func (w *WindowsOsOperations) StartCmd(cmd *exec.Cmd) error {
+	return cmd.Start()
+}
+
+func (w *WindowsOsOperations) KillProcess(process *os.Process) error {
+	return process.Kill()
+}
+
 type WinProcess struct {
 	cmd     *exec.Cmd
 	program string
 	args    []string
+	osOps   OsOperations
 }
 
 // Started implements Process.
@@ -20,19 +61,26 @@ func (w *WinProcess) Started() bool {
 		return false
 	}
 
-	process, err := os.FindProcess(w.cmd.Process.Pid)
+	return w.isProcessRunning(w.cmd.Process.Pid)
+}
+
+// isProcessRunning checks if a process with the given PID is running
+func (w *WinProcess) isProcessRunning(pid int) bool {
+	process, err := w.osOps.FindProcess(pid)
 	if err != nil {
 		return false
 	}
 
-	err = process.Signal(syscall.Signal(0))
+	err = w.osOps.Signal(process, syscall.Signal(0))
 	return err == nil
 }
 
-// NewRealProcess initializes a process with a given command.
+// NewWinProcess initializes a process with a given command.
 func NewWinProcess(command string, args ...string) *WinProcess {
 	return &WinProcess{
-		program: command, args: args,
+		program: command,
+		args:    args,
+		osOps:   &WindowsOsOperations{},
 	}
 }
 
@@ -47,13 +95,10 @@ func (w *WinProcess) Start() error {
 	if w.program == "" {
 		return fmt.Errorf("program is empty")
 	}
-	w.cmd = exec.Command(w.program, w.args...)
-	w.cmd.SysProcAttr = &syscall.SysProcAttr{
-		CreationFlags: syscall.CREATE_NEW_PROCESS_GROUP, // Create a new process group
-		HideWindow:    false,                            // Prevents the Notepad window from showing
+	w.cmd = w.osOps.CreateCommand(w.program, w.args...)
+	w.osOps.SetSysProcAttr(w.cmd)
 
-	}
-	err := w.cmd.Start()
+	err := w.osOps.StartCmd(w.cmd)
 	if err != nil {
 		return fmt.Errorf("failed to start process %s: %w", w.program, err)
 	}
@@ -66,7 +111,7 @@ func (w *WinProcess) Start() error {
 func (w *WinProcess) Stop() error {
 	if w.cmd != nil && w.cmd.Process != nil {
 		pid := w.cmd.Process.Pid
-		err := w.cmd.Process.Kill()
+		err := w.osOps.KillProcess(w.cmd.Process)
 		if err != nil {
 			return fmt.Errorf("failed to kill process %s (PID: %d): %w", w.program, pid, err)
 		}
