2222b74b50ef02cb22a18bcd3a19e8f531d94c76
juandeaglio <casualgamer105@yahoo.com>
2025-04-05 14:52:57 -0400

    .r Removed duplicated brokenOsOperations

--- tests/unit/process/process_test.go
+++ tests/unit/process/process_test.go
@@ -1,41 +1,12 @@
-package processtest_test
+package processtest
 
 import (
-	"fmt"
-	"os"
-	"os/exec"
-	"syscall"
-
 	"testing"
 
 	"minecraftremote/src/process"
 
 	"github.com/stretchr/testify/assert"
 )
-
-type brokenOsOperations struct{}
-
-func (b *brokenOsOperations) FindProcess(pid int) (*os.Process, error) {
-	return nil, fmt.Errorf("broken")
-}
-
-func (b *brokenOsOperations) Signal(process *os.Process, signal syscall.Signal) error {
-	return fmt.Errorf("broken")
-}
-
-func (b *brokenOsOperations) CreateCommand(program string, args ...string) *exec.Cmd {
-	return exec.Command(program, args...)
-}
-
-func (b *brokenOsOperations) SetSysProcAttr(cmd *exec.Cmd) {}
-
-func (b *brokenOsOperations) StartCmd(cmd *exec.Cmd) error {
-	return fmt.Errorf("broken")
-}
-
-func (b *brokenOsOperations) KillProcess(process *os.Process) error {
-	return fmt.Errorf("broken")
-}
 
 func TestProcess(t *testing.T) {
 	fakeProcess := process.NewProcess(&process.FakeOsOperations{}, "fake", "args")
