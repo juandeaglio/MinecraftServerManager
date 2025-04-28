da947d3ac808f5f19ca623ca6ccafe4ba27b8a19
juandeaglio <casualgamer105@yahoo.com>
2025-04-28 14:35:01 -0400

    .r Re-factored test to focus on behavior validation instead of JSON validation.

--- tests/integration/godogs/features/control_server.feature
+++ tests/integration/godogs/features/control_server.feature
@@ -3,7 +3,7 @@ Feature: Control the minecraft server
   Scenario: Client asks the server for the status
     Given the Minecraft server is running and ready
     When a client requests the server status
-    Then the system returns a status response indicating "online" along with the current player count
+    Then the system returns a status response
 
   Scenario: Client starts the server
     Given the Minecraft server isn't started
--- tests/integration/godogs/status_steps/status_steps.go
+++ tests/integration/godogs/status_steps/status_steps.go
@@ -2,15 +2,12 @@ package status_steps
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
-	"io"
 	"minecraftremote/src/process"
 	"minecraftremote/src/rcon"
 	"minecraftremote/tests/integration/godogs/constants"
 	"minecraftremote/tests/integration/godogs/test_infrastructure"
 	"net/http"
-	"strings"
 
 	"github.com/cucumber/godog"
 )
@@ -35,31 +32,14 @@ func (c *checkServerFeature) theMinecraftProcessIsRunning() error {
 
 func (c *checkServerFeature) aClientRequestsMinecraftProcessStatus() error {
 	c.resp, _ = http.Get(url)
-	statusCode := c.resp.StatusCode
-	if statusCode == 200 {
+	return nil
+}
+
+func (c *checkServerFeature) theAPIReturnsMinecraftProcessStatus() error {
+	if c.resp.StatusCode == 200 {
 		return nil
 	}
 	return fmt.Errorf("The client was unable to get the Minecraft process status correctly")
-}
-
-func (c *checkServerFeature) theAPIReturnsMinecraftProcessStatusWithPlayerCount() error {
-	contentType := c.resp.Header.Get("Content-Type")
-	if !strings.Contains(contentType, "application/json") {
-		return fmt.Errorf("expected content type to be application/json but got %s", contentType)
-	}
-
-	defer c.resp.Body.Close()
-	body, err := io.ReadAll(c.resp.Body)
-	if err != nil {
-		return fmt.Errorf("error reading response body: %v", err)
-	}
-
-	var response map[string]interface{}
-	if err := json.Unmarshal(body, &response); err != nil {
-		return fmt.Errorf("error parsing JSON response: %v", err)
-	}
-
-	return nil
 }
 
 // We use a lot of specific stubs/fakes here. The question is why?
@@ -88,5 +68,5 @@ func ClientAsksTheServerForTheStatusScenarioContext(s *godog.ScenarioContext) {
 
 	s.Given(`^the Minecraft server is running and ready$`, c.theMinecraftProcessIsRunning)
 	s.When(`^a client requests the server status$`, c.aClientRequestsMinecraftProcessStatus)
-	s.Then(`^the system returns a status response indicating "online" along with the current player count$`, c.theAPIReturnsMinecraftProcessStatusWithPlayerCount)
+	s.Then(`^the system returns a status response$`, c.theAPIReturnsMinecraftProcessStatus)
 }
